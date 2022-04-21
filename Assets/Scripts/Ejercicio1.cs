ef6fb22e71babb86327a9f4c012847a318d25709
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-04-21 14:34:12 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio1.cs
@@ -0,0 +1,45 @@
+﻿//Realiza un programa que mueste los números entre dos enteros ingresados por el usuario;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Son iguales");
+        }
+
+        else if (num1 > num2)
+        {
+            while (num1 >= num2)
+            {
+                Debug.Log(num2);
+                num2++;
+            }
+        }
+
+        else
+        {
+            while (num2 >= num1)
+            {
+                Debug.Log(num1);
+                num1++;
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,29 @@
+﻿//Realizar un programa que muestre los números del 1 al 100 usando la instrucción while.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    int counter = 0;
+    int counterMax = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter <= counterMax)
+        {
+            Debug.Log(counter);
+            counterMax++;
+        }
+
+        Debug.Log("Terminó el while");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
